875e856ed40620df73aab08474b05a3751b23ba6
Erin Chuah <chuah.erin@gmail.com>
2019-04-15 11:30:59 +0800

    fixed url https

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-//app.js
 App({
   onLaunch: function () {
-    const host = 'https://yiqiwu.shanghaiwogeng.com/'
+    const host = this.globalData.host
     console.log('processing to login')
     wx.login({
       success: (res) => {
@@ -18,17 +17,13 @@ App({
         wx.getSetting({
           success: res => {
             if (res.authSetting['scope.userInfo']) {
-              // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
               wx.getUserInfo({
                 success: res => {
-                  // 可以将 res 发送给后台解码出 unionId
                   this.globalData.userInfo = res.userInfo
                   wx.redirectTo({
                     url: '/pages/main/main',
                   })
 
-                  // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                  // 所以此处加入 callback 以防止这种情况
                   if (this.userInfoReadyCallback) {
                     this.userInfoReadyCallback(res)
                     wx.switchTab({
@@ -44,6 +39,7 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    host: `https://yiqiwu.wogengapp.cn/`,
+    version: `api/v1/`
   }
 })
--- pages/activities/activities.js
+++ pages/activities/activities.js
@@ -1,10 +1,5 @@
-// pages/activities/activities.js
 const app = getApp();
 Page({
-
-  /**
-   * Page initial data
-   */
   data: {
 
   },
@@ -14,15 +9,13 @@ Page({
     
     const userId = app.globalData.userId;
 
-    // Get api data
     wx.request({
-      url: `https://yiqiwu.wogengapp.cn/api/v1/users/${userId}`,
+      url: app.globalData.host + app.globalData.version + `users/${userId}`,
       // url: `http://localhost:3000/api/v1/users/${userId}`,
       method: 'GET',
       success(u) {
         const bookings = u.data.bookings;
       
-        // Update local data
         self.setData({
           bookings: bookings
         });
